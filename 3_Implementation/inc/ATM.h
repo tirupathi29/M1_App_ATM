4ba1831c56293009e9c8b5c12cde75713d1763c8
Saba Begum <94221735+sababegums@users.noreply.github.com>
2021-11-23 15:38:06 +0530

    Update ATM.h

--- 3_Implementation/inc/ATM.h
+++ 3_Implementation/inc/ATM.h
@@ -1,1 +1,57 @@
+/**
+ * @file feature.h
+ * @author Saba Begum ()
+ * @brief ATM function prototypes
+ * @version 0.1
+ * @date 2021-11-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
+
+
+#ifndef FEATURE_H
+#define FEATURE_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+
+/*
+  node structure for implementing a queue to store transaction history
+*/
+typedef struct node {
+    char statement[50];
+    struct node* link;
+} node;
+
+
+/*
+  node structure for implementing a queue to store transaction history
+*/
+typedef struct node {
+    char statement[50];
+    struct node* link;
+} node;
+
+/*
+  ATM function prototypes
+*/
+void pinGeneration(void);
+int checkPin(void);
+void showBalance(int *);
+void depositMoney(node **, int *);
+void withdrawMoney(node **, int *);
+void saveHistory(node **, char *);
+void removeHistory(node **);
+void showHistory(node **);
+
+
+#endif
+
+/**
+ * @brief end of function declaration
+ * 
+ */
